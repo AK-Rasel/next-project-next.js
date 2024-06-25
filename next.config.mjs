8dd43f70ef14d9cfcffd1eb1a528f554841dc5f0
AK-Rasel <raselak101010@gmail.com>
2024-06-25 12:19:53 +0600

    file-setup and install punycode and fix deprecation warning punycode

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { resolve } from 'path';
+
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Fixes npm packages that depend on `punycode` module
+      config.resolve.alias['punycode'] = resolve('node_modules/punycode/');
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
